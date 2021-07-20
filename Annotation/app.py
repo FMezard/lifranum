e79f1eb7321f9c48a42be6b822e94a3347f460b2
fanny.mezard <fanny.mezard@hotmail.fr>
2021-07-20 14:45:44 +0200

    passage à pandas

--- Annotation/app.py
+++ Annotation/app.py
@@ -4,7 +4,9 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
+import dash_table
 import pandas as pd
+import numpy as np
 import plotly.express as px
 from dash.dependencies import Input, Output
 import json
@@ -14,9 +16,12 @@ from dash_html_template import Template
 from pprint import pprint
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.YETI])
+pd.set_option('display.max_colwidth', None)
 
 def json_to_dict(blogposts_files) :
     # Returns a dict with the id of the post as keys
+    # Fonction à adapter pour tjs avoir les données au bon format --> Sauf si on fait les requêtes au fur et à mesure
+    # dans MongoDB mais je sais pas comment
     with open(blogposts_files, "r", encoding="utf-8") as file:
         blogposts = json.load(file)
         posts = {}
@@ -39,73 +44,75 @@ def json_to_dict(blogposts_files) :
             }
     return posts
 
+def json_to_dataframe(blogposts_files) :
+    with open(blogposts_files, "r", encoding="utf-8") as file:
+        blogposts = json.load(file)
+        # ajouter les auteurs des commentaires
+        columns = ["blog_id", "published", "url", "title", "content", "labels", "author_id", "author_displayName", "updated"]
+    dataframe = {}
+    for c in columns:
+        dataframe[c] = []
+    for b in blogposts:
+        title = b["title"]
+        if b["title"] == "":
+            title = "Sans Titre"
+
+        dataframe["blog_id"].append(b["blog"]["id"])
+        dataframe["published"].append(b["published"])
+        dataframe["updated"].append(b.get("updated", ""))
+        dataframe["url"].append(b["url"])
+        dataframe["title"].append(title)
+        dataframe["content"].append(b["content"])
+        dataframe["labels"].append(b["labels"])
+        dataframe["author_id"].append(b["author"]["id"])
+        dataframe["author_displayName"].append(b["author"]["displayName"])
+    df = pd.DataFrame(dataframe)
+    return df
+
+
 
 blogposts = json_to_dict("posts.json")
+blogposts_df = json_to_dataframe("posts.json")
+blogposts_df['id'] = blogposts_df.index
 
-def get_blogs(blogposts):
-    table_blogs = html.Table(
-        children=[],
-        id="table_blogs"
-    )
-    for k in blogposts:
-        title = blogposts[k]["title"]
-        if title == '':
-            title = 'Sans Titre'
-        tr_blog = html.Tr(children=[])
-        tr_blog.children.append(html.Td(blogposts[k]["blog_id"]))
-        tr_blog.children.append(html.Td(title))
-        tr_blog.children.append(html.Td(blogposts[k]["url"]))
-        tr_blog.children.append(html.Td(blogposts[k]["author_displayName"]))
 
-        table_blogs.children.append(dcc.Link(tr_blog, href='display_post'))
-    return table_blogs
 
+
+@app.callback(Output(component_id='post-content', component_property='children'),
+              Input(component_id='table_blogs', component_property='selected_row_ids'))
+
+def display_one_post(selected_row_ids):
+    post = ""
+    if selected_row_ids:
+
+        dff = blogposts_df.loc[selected_row_ids]
+        post = html.Div(id='post-content-child',
+                 children=[
+                     html.H2(dff['title']),
+                     html.H3(dff['author_displayName']),
+                     html.Div((Template.from_string(f'<div>{dff.iloc[0]["content"]}</div>')))
+                 ])
+    return post
 
 @app.callback(
     Output(component_id='blogs', component_property='children'),
     Input(component_id='searchbox', component_property='value')
 )
 def filter_results(input_value):
-    filtered_posts = list()
-    for post in blogposts:
-        # TODO Ajouter des .lowers
-        if any([input_value in blogposts[post][k] for k in blogposts[post]]):
-            filtered_posts.append(post)
-
-    table_blogs = html.Table(
-        children=[],
-        id="table_blogs"
-    )
-    for i in filtered_posts:
-        tr_blog = html.Tr(children=[])
-        tr_blog.children.append(html.Td(dcc.Link(i, href=f'/{i}')))
-        tr_blog.children.append(html.Td(blogposts[i]["title"]))
-        tr_blog.children.append(html.Td(blogposts[i]["url"]))
-        tr_blog.children.append(html.Td(blogposts[i]["author_displayName"]))
-        table_blogs.children.append(tr_blog)
-    return table_blogs
-
-@app.callback(Output('post-content', 'children'),
-              Input('url', 'pathname'))
-def display_one_post(pathname):
-    displayed_post = list(blogposts.keys())[0]
-    if pathname:
-        for blog in blogposts:
-            if blog in pathname:
-                displayed_post = blogposts[blog]
-                break
-
-        post = html.Div(children=[
-            html.H2(displayed_post['title']),
-            html.H2(displayed_post['author_displayName']),
-            html.Div(["Labels choisis par l'éditeur : "]),
-            html.Div(displayed_post['labels']),
-            html.Div((Template.from_string(f'<div>{displayed_post["content"]}</div>')))
-
-        ])
-        return post
-
-
+    mask = np.column_stack([blogposts_df[col].str.contains(input_value, na=False) for col in blogposts_df.loc[:, blogposts_df.columns != 'id']])
+    filtered_df = blogposts_df.loc[mask.any(axis=1)]
+    table = dash_table.DataTable(
+        id='table_blogs',
+        filter_action='native',
+        sort_action='native',
+        row_selectable='single',
+        columns=[{"name": "ID", "id": "blog_id"},
+                 {"name": "Titre", "id": "title"},
+                 {"name": "Mots-clés", "id": "labels"},
+                 {"name": "Auteur.e", "id": "author_displayName"}
+                 ],
+        data=filtered_df.to_dict('records'))
+    return table
 
 app.layout = html.Div(children=[
     dcc.Location(id='url', refresh=False),
@@ -117,9 +124,9 @@ app.layout = html.Div(children=[
         value=""
     ),
     html.Div(id='blogs'),
-    html.Div(id='post-content')
+    dash_table.DataTable(id='table_blogs'),
+    html.Div(id="post-content")
 ])
-
 
 
 if __name__ == '__main__':
